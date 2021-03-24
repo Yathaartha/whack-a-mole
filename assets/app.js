0146ce9ca6daa4f64ed193a450a608488f8a79ff
Yathaartha <maharjanyathaartha017@gmail.com>
2021-03-24 21:14:53 +0545

    finished game logic

--- assets/app.js
+++ assets/app.js
@@ -1,71 +1,46 @@
-const canvas = document.querySelector("canvas");
+const moles = document.querySelectorAll("img");
 
-const ctx = canvas.getContext("2d");
-
-const scoreCount = document.getElementById("score");
-const highScoreCount = document.getElementById("highscore");
-
-let primary = "#011627";
-let secondary = "#AE17DB";
-let highscore = 0;
-let nameHighScore = "";
-let timer;
-let sec = 0;
-
-const grid = 32; //size of 1 cell
-let count = 0; //to manipulate frames per sec
 let score = 0;
+let milisec = 0;
+let sec = 0;
+let temp = 0;
 
-let moles = {
-  x: grid * 10, //starting point of food
-  y: grid * 10,
-};
-
-function startTimer() {
-  timer = setInterval(timeHandler, 1000);
+function spawnMole() {
+  temp = getRandomInt(0, 9);
+  moles[temp].classList.add("active");
 }
 
-function timeHandler() {
-  sec++;
-  createMole();
+function despawnMole() {
+  moles[temp].classList.remove("active");
+  milisec = 99;
 }
 
-function createMole() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = primary;
-  ctx.fillRect(moles.x, moles.y, grid - 1, grid - 1);
-  moles.x = getRandomInt(0, 24) * grid;
-  moles.y = getRandomInt(0, 14) * grid;
-  console.log(getRelativeCoords(onclick));
-}
-
-function getRelativeCoords(event) {
-  return { x: event.offsetX, y: event.offsetY };
-}
+moles.forEach((e) => {
+  e.addEventListener("click", () => {
+    if (e.classList.contains("active")) {
+      score++;
+      despawnMole();
+      console.log(score);
+    }
+  });
+});
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function relMouseCoords(event) {
-  var totalOffsetX = 0;
-  var totalOffsetY = 0;
-  var canvasX = 0;
-  var canvasY = 0;
-  var currentElement = this;
-
-  do {
-    totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
-    totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
-  } while ((currentElement = currentElement.offsetParent));
-
-  canvasX = event.pageX - totalOffsetX;
-  canvasY = event.pageY - totalOffsetY;
-
-  return { x: canvasX, y: canvasY };
+function startTimer() {
+  setInterval(timeHandler, 10);
 }
-HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
 
-//starts the game
+function timeHandler() {
+  milisec++;
+  if (milisec === 100) {
+    spawnMole();
+  }
+  if (milisec === 150) {
+    despawnMole();
+  }
+}
+
 startTimer();
-// requestAnimationFrame(startGame); //sets the fps to 60
